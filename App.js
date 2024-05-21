cb6c65577f6b582a2c65e7f3c471a7d032af9fa1
benshidanny11 <benshidanny11@mail.com>
2024-05-21 16:56:19 +0200

    Implement coding challenge

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const employees = [{ name: 'Danny Benshi', age: 29, occupation: 'Software engineer', gender: 'Male', },
+  { name: 'Emmy Gaga', age: 22, occupation: 'Driver', gender: 'Male', },
+  { name: 'Claudia Umulisa', age: 19, occupation: 'Sudent', gender: 'Femaile', },
+  { name: 'Agnes Uwimana', age: 50, occupation: 'Retirement', gender: 'Female', },
+  { name: 'Eric Rukundo', age: 27, occupation: 'University assistant lecturer', gender: 'Male', }]
+
+  const [employeeName, setEmployeeName] = useState('');
+
+
+  const FilterData = ({ item }) => {
+    if (employeeName == "") {
+      return (<View><Text>{item.name}</Text></View>)
+    }
+
+    if (item.name.toLowerCase().includes(employeeName.toLocaleLowerCase())) {
+      return (<View><Text>{item.name}</Text></View>)
+    }
+  }
+  const [emp, setEmp] = useState(employees)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView />
+      <TextInput placeholder='Type name of employee' onChangeText={(txt) => setEmployeeName(txt)} />
+      <View style={styles.listContainer}>
+        <FlatList data={emp} renderItem={({ item }) => {
+          return <FilterData item={item} />
+        }} keyExtractor={(item, index) => item.name + index} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    flex: 1
+
   },
+  listContainer: {
+    //backgroundColor:'red',
+    flex: 1
+  },
+  itemStyle: {
+    backgroundColor: 'red'
+  }
 });
